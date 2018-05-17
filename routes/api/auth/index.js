b1b00c599d530854deb02f3ea6824c703c63116b
padakim <bada013@naver.com>
2018-05-17 23:48:18 +0900

    로그인, 회원가입, 로그아웃 API 생성

--- routes/api/auth/index.js
+++ routes/api/auth/index.js
@@ -1,16 +1,52 @@
 const router = require('express').Router();
 const ctrl = require('./ctrl');
 const passport = require('passport');
+const middle = require('../../../lib/middlewares/isAuthenticate');
+const utils = new (require('../../../lib/common/utils'))();
 
-
+/**
+ * POST: /api/auth/signin Body: email, password
+ * 로그인 API, 성공: GET /api/auth/signin, 실패: GET /api/auth/signin
+ */
 router.post('/signin', passport.authenticate('signIn', {
-    successRedirect: '/',
-    failureRedirect: '/dashboard'
+    successRedirect: '/api/auth/signin',
+    failureRedirect: '/api/auth/signin'
 }));
 
+/**
+ * POST /api/auth/signup Body: email, password
+ * 회원가입 API, 성공: GET /api/auth/signup, 실패: GET /api/auth/signup
+ */
 router.post('/signup', passport.authenticate('signUp',{
-    successRedirect:'/',
-    failureRedirect: '/dashboard',
+    successRedirect: '/api/auth/signup',
+    failureRedirect: '/api/auth/signup',
 }));
+
+/**
+ * 로그인 성공 or 실패시 호출하는 API
+ */
+router.get('/signin',(req,res,error)=>{
+
+    if(req.session.passport){
+        utils.resJson(res, 200, "Success Sign In");
+    }else{
+        utils.resJson(res, 404, "Failed Sign In");
+    }
+})
+/**
+ * 회원가입 성공 or 실패시 호출하는 API
+ */
+router.get('/signup',(req,res,error)=>{
+    if (error){
+        utils.resJson(res, 404, error);
+    }else{
+        utils.resJson(res, 200, "Success Sign Up");
+    }
+    
+})
+
+// GET /api/auth/signout 로그아웃 - 세션파괴
+router.get('/signout',middle.authenticate,ctrl.signOut)
+router.get('/get', middle.authenticate,ctrl.getItem);
 
 module.exports = router;
