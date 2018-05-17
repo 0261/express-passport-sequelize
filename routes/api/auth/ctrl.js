e1aa7f88c4e55ce514789593a8c1ee675608544a
padakim <bada013@naver.com>
2018-05-17 23:48:02 +0900

    로그아웃 함수 추가

--- routes/api/auth/ctrl.js
+++ routes/api/auth/ctrl.js
@@ -2,9 +2,27 @@
 require('debugs/init');
 const debug = require('debug')('auth');
 const passport = require('passport');
-
+const utils = new (require('../../../lib/common/utils'))();
 const ctrl = {
-
+    getItem(req,res){
+        
+    },
+    /**
+     * Sign Out Function
+     * @param {*} req Request
+     * @param {*} res Response
+     * 
+     * 세션 파괴
+     */
+    signOut(req,res){
+        req.session.destroy((error)=>{
+            if(error){
+                utils.resJson(res, 404, error);
+            }else{
+                utils.resJson(res, 200, "Success Sign Out");
+            }
+        })
+    }
 }
 
 module.exports = ctrl;
